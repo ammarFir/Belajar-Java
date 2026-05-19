adbcb826719093ce0e11cf2aabd6ce04720489c3
abcdefucky0 <ammaralqarobi363@gmail.com>
2026-05-20 00:22:28 +0800

    Initial Java Learning Project

---
+++ src/MyFolder/Latihan/printHelloWorld1.java
@@ -0,0 +1,9 @@
+package MyFolder.Latihan;
+
+public class printHelloWorld1 {
+    public static void main(String[] args) {
+        // fungsi utama
+        System.out.println("Testing Hello World");
+        // print text
+    }
+}
