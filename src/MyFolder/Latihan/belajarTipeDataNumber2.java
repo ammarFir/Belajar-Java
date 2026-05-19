294487a588e4cb43ee59efeaddd7198a8f850c85
abcdefucky0 <ammaralqarobi363@gmail.com>
2026-05-20 00:35:12 +0800

    Belajar Tipe Data Number

---
+++ src/MyFolder/Latihan/belajarTipeDataNumber2.java
@@ -0,0 +1,26 @@
+package MyFolder.Latihan;
+
+public class belajarTipeDataNumber2 {
+    public static void main(String[] args) {
+
+        byte iniByte = 100;
+        short iniShort = 1000;
+        int iniInt = 1000000;
+        long iniLong = 1000000000L;
+
+        float iniFloat = 10.10F;
+        // boleh f kecil ato F besar
+        double iniDouble = 10.10;
+
+        int decimalInt = 34;
+        int hexaDecimal = 0xFFFFFF;
+        int bynaryDecimal = 0b101010;
+        // Literal
+
+        // Underscore cuma sebagai Pemisah
+        int amount = 1_000_000;
+        int angkaPemisah = 1_0_0_0;
+        // kek gin juga boleh
+
+    }
+}
--- src/MyFolder/Latihan/printHelloWorld1.java
+++ src/MyFolder/Latihan/printHelloWorld1.java
@@ -4,6 +4,6 @@ public class printHelloWorld1 {
     public static void main(String[] args) {
         // fungsi utama
         System.out.println("Testing Hello World");
-        // print text
+        // print textgit
     }
 }
