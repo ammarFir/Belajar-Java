49a7ba52c25bf8c878301dc1065cb35212748483
abcdefucky0 <ammaralqarobi363@gmail.com>
2026-06-18 19:40:46 +0800

    challenge method

--- src/MyFolder/Latihan/Challenge11.java
+++ src/MyFolder/Latihan/Challenge11.java
@@ -1,5 +1,37 @@
 package myfolder.latihan;
 
+import java.util.*;
+
 public class Challenge11 {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        int panjang, lebar;
+
+        System.out.print("Masukkan Panjang :");
+        panjang = input.nextInt();
+
+        System.out.print("Masukkan Lebar :");
+        lebar = input.nextInt();
+
+        hitungKeliling(panjang, lebar);
+
+        int hasilKeliling = hitungKeliling(panjang, lebar);
+        cetakHasil(hasilKeliling);
+
+    }
+
+    public static int hitungKeliling(int panjang, int lebar) {
+        int keliling;
+        keliling = 2 * (panjang + lebar);
+        return keliling;
+    }
+
+    public static void cetakHasil(int hasilKeliling) {
+
+        System.out.println("Hasil Keliling :" + hasilKeliling);
+
+    }
 
 }
