5f2541d573fa3fcbf92da42da21d560b42aeb469
abcdefucky0 <ammaralqarobi363@gmail.com>
2026-06-19 22:16:33 +0800

    Challenge 16

---
+++ src/MyFolder/Latihan/Challenge16.java
@@ -0,0 +1,11 @@
+package myfolder.latihan;
+
+public class Challenge16 {
+    public static void main(String[] args) {
+        sapaDunia();
+    }
+
+    public static void sapaDunia() {
+        System.out.println(" heelo geyz");
+    };
+}
