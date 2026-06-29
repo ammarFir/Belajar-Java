71f72e68765a132ba7787ca3d6942c4872ecc019
abcdefucky0 <ammaralqarobi363@gmail.com>
2026-06-29 14:40:51 +0800

    get set

--- src/mvc/model/Nasabah.java
+++ src/mvc/model/Nasabah.java
@@ -10,4 +10,36 @@ public class Nasabah {
         this.saldo = saldo;
     }
 
+    public String getNama() {
+        return nama;
+    }
+
+    public String getNoRekening() {
+        return noRekening;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setNama(String nama) {
+        if (nama == null) {
+            System.out.println("Nama tidak boleh kosong");
+        } else {
+            this.nama = nama;
+        }
+    }
+
+    public void setNoRekening(String noRekening) {
+        this.noRekening = noRekening;
+    }
+
+    public void setSaldo(double saldo) {
+        if (saldo < 0) {
+            System.out.println("Mohon maaf saldo tidak boleh kosong");
+        } else {
+            this.saldo = saldo;
+        }
+    }
+
 }
