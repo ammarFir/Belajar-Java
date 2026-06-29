7f4176ed3a6005aa2e46ff68da445ef8f15b5890
abcdefucky0 <ammaralqarobi363@gmail.com>
2026-06-29 14:34:42 +0800

    model nasabah:

---
+++ src/mvc/model/Nasabah.java
@@ -0,0 +1,13 @@
+package mvc.model;
+
+public class Nasabah {
+    private String nama, noRekening;
+    private double saldo;
+
+    public Nasabah(String nama, String noRekening, double saldo) {
+        this.nama = nama;
+        this.noRekening = noRekening;
+        this.saldo = saldo;
+    }
+
+}
