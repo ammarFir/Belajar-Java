f823ab869a55bb3845e43635a683707839b70c04
abcdefucky0 <ammaralqarobi363@gmail.com>
2026-07-02 22:52:05 +0800

    comiit

--- src/mvc/view/MobilView.java
+++ src/mvc/view/MobilView.java
@@ -5,8 +5,10 @@ import mvc.model.Mobil;
 public class MobilView {
 
     public void tampilkanMobil(Mobil mobil) {
-        System.out.println("Masukkan merk mobil : " + mobil.getMerk());
-        System.out.println("Masukkan tahun mobil : " + mobil.getTahun());
-        System.out.println("Masukkan kecepatan mobil : " + mobil.getKecepatan());
+        System.out.println("  --- DATA MOBIL ---  ");
+        System.out.println("Merk : " + mobil.getMerk());
+        System.out.println("Warna : " + mobil.getWarna());
+        System.out.println("Tahun : " + mobil.getTahun());
+        System.out.println("Masukkan kecepatan mobil : " + mobil.getKecepatan() + "km/h");
     }
 }
