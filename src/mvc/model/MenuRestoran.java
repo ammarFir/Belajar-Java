660cd4ea60ce4a6984d53b430401af114cc381bf
abcdefucky0 <ammaralqarobi363@gmail.com>
2026-07-06 22:04:40 +0800

    constructor

--- src/mvc/model/MenuRestoran.java
+++ src/mvc/model/MenuRestoran.java
@@ -1,5 +1,13 @@
 package mvc.model;
 
 public class MenuRestoran {
+    private String namaMenu;
+    private double harga;
+    private int rating;
 
+    public MenuRestoran(String namaMenu, double harga, int rating) {
+        this.namaMenu = namaMenu;
+        this.harga = harga;
+        this.rating = rating;
+    }
 }
