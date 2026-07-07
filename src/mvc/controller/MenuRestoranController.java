e2ae57a28bb13acfac7b274aa644576bac57a7fd
abcdefucky0 <ammaralqarobi363@gmail.com>
2026-07-07 21:32:53 +0800

    controller

--- src/mvc/controller/MenuRestoranController.java
+++ src/mvc/controller/MenuRestoranController.java
@@ -2,7 +2,21 @@ package mvc.controller;
 
 import mvc.model.MenuRestoran;
 import mvc.view.MenuView;
+import java.util.*;
 
 public class MenuRestoranController {
+    public void tampilkanMenu() {
+        Scanner input = new Scanner(System.in);
 
+        String menu;
+        do {
+            System.out.print("Masukkan nama menu : ");
+            menu = input.nextLine();
+
+            if (menu.trim().equals("")) {
+                System.out.print("Nama Menu tidak boleh kosong !");
+            }
+        } while (menu.trim().equals(""));
+
+    }
 }
