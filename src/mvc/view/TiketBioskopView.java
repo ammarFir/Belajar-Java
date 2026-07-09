cc9aa540f50d557885f2565f4114addd2f045d26
abcdefucky0 <ammaralqarobi363@gmail.com>
2026-07-09 22:07:59 +0800

    view tiket biosko 1p

--- src/mvc/view/TiketBioskopView.java
+++ src/mvc/view/TiketBioskopView.java
@@ -1,5 +1,14 @@
 package mvc.view;
 
-public class TiketBioskopView {
+import mvc.model.TiketBioskop;
 
+public class TiketBioskopView {
+    public void tampilkanTiket(TiketBioskop tBioskop) {
+        System.out.print("===Data Tiket===");
+        System.out.print("Film : " + tBioskop.getFilm());
+        System.out.print("Hari : " + tBioskop.getHari());
+        System.out.print("Jam : " + tBioskop.getJam());
+        System.out.print("Harga Awal : " + tBioskop.getHarga());
+        System.out.print("Harga Akhir " + tBioskop.hitungHargaAkhir());
+    }
 }
