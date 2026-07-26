393db231fb97dccca97e12bcbb3f9089464b9a91
abcdefucky0 <ammaralqarobi363@gmail.com>
2026-07-26 19:29:32 +0800

    model produk diskon

---
+++ src/mvc/model/ProdukDiskon.java
@@ -0,0 +1,53 @@
+package mvc.model;
+
+public class ProdukDiskon {
+
+    // deklarasi
+    private String namaProduk, kategori;
+    private double harga;
+    private int jumlahBeli;
+
+    // constructor
+    public ProdukDiskon(String namaProduk, String kategori, double harga, int jumlahBeli) {
+        this.namaProduk = namaProduk;
+        this.kategori = kategori;
+        this.harga = harga;
+        this.jumlahBeli = jumlahBeli;
+    }
+
+    public String getNamaProduk() {
+        return namaProduk;
+    }
+
+    public String getKategori() {
+        return kategori;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+
+    public int getjumlahBeli() {
+        return jumlahBeli;
+    }
+
+    public void setNamaProduk(String namaProduk) {
+        this.namaProduk = namaProduk;
+    }
+
+    public void setKategori(String kategori) {
+        this.kategori = kategori;
+    }
+
+    public void setHarga(double harga) {
+        this.harga = harga;
+    }
+
+    public void setJumlahBeli(int jumlahBeli) {
+        this.jumlahBeli = jumlahBeli;
+    }
+
+    public double hitungTotalBayar() {
+
+    }
+}
