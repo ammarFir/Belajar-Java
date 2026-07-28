821f14d4c55f4b58e0e6697878caeb46a471046c
abcdefucky0 <ammaralqarobi363@gmail.com>
2026-07-28 20:19:48 +0800

    1

--- src/mvc/controller/ProdukDiskonController.java
+++ src/mvc/controller/ProdukDiskonController.java
@@ -6,4 +6,40 @@ import java.util.Scanner;
 
 public class ProdukDiskonController {
 
+    public void tampilkanProdukDiskon() {
+        Scanner input = new Scanner(System.in);
+
+        // iterasi nama produk
+        String namaProduk;
+        do {
+            System.out.println("Masukkan nama produk : ");
+            namaProduk = input.nextLine();
+
+            if (namaProduk.trim().equals("")) {
+                System.out.println("Nama Produk Tidak Boleh Kosong");
+            }
+
+        } while (namaProduk.trim().equals(""));
+
+        // iterasi kategori
+        String[] kategoriValid = { "Elektronik", "Pakaian", "Makanan" };
+        String kategori;
+        Boolean valid;
+        do {
+            valid = false;
+            System.out.println("Masukkan kategori produk : ");
+            kategori = input.nextLine();
+
+            for (int i = 0; i < kategoriValid.length; i++) {
+                if (kategori.equalsIgnoreCase(kategoriValid[i])) {
+                    kategori = kategoriValid[i];
+                    valid = true;
+                }
+            }
+        } while (!valid);
+
+
+        ProdukDiskonView
+    }
+
 }
