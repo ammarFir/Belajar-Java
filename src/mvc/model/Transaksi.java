11ad87683f1343ab822bc90f793312df15289fb6
abcdefucky0 <ammaralqarobi363@gmail.com>
2026-08-20 00:40:19 +0800

    transaksi

---
+++ src/mvc/model/Transaksi.java
@@ -0,0 +1,47 @@
+package mvc.model;
+
+public class Transaksi {
+    private String namaBarang;
+    private int jumlah;
+    private double hargaSatuan;
+    private String metodeBayar;
+
+    public Transaksi(String namaBarang, int jumlah, double hargaSatuan, String metodeBayar) {
+        this.namaBarang = namaBarang;
+        this.jumlah = jumlah;
+        this.hargaSatuan = hargaSatuan;
+        this.metodeBayar = metodeBayar;
+    }
+
+    public String getNamaBarang() {
+        return namaBarang;
+    }
+
+    public int getjumlah() {
+        return jumlah;
+    }
+
+    public double hargaSatuan() {
+        return hargaSatuan;
+    }
+
+    public String metodeBayar() {
+        return metodeBayar;
+    }
+
+    public void setNamaBarang(String namaBarang) {
+        this.namaBarang = namaBarang;
+    }
+
+    public void setJumlah(int jumlah) {
+        this.jumlah = jumlah;
+    }
+
+    public void setHargaSatuan(double hargaSatuan) {
+        this.hargaSatuan = hargaSatuan;
+    }
+
+    public void setMetodeBayar() {
+        this.metodeBayar = metodeBayar;
+    }
+}
