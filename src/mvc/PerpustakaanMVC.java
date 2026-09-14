05eb0fa21dc774c4c5069a5294d58fc90f04f8e4
abcdefucky0 <ammaralqarobi363@gmail.com>
2026-09-14 19:25:31 +0800

    perpusmvc inherit

---
+++ src/mvc/PerpustakaanMVC.java
@@ -0,0 +1,10 @@
+package mvc;
+import  mvc.controller.PerpustakaanController;
+
+public class PerpustakaanMVC {
+    public static void main(String[] args) {
+         PerpustakaanController perpustakaanController = new PerpustakaanController();
+    perpustakaanController.tampilkanMenu();
+    }
+   
+}
--- src/mvc/controller/PerpustakaanController.java
+++ src/mvc/controller/PerpustakaanController.java
@@ -1,14 +1,21 @@
 package mvc.controller;
 import mvc.model.BukuPerpus;
 import  mvc.model.ItemPerpustakaan;
+import mvc.model.MajalahPerpus;
 import  mvc.view.PerpustakaanView;
 import  java.util.*;
 
 public class PerpustakaanController {
     ArrayList <ItemPerpustakaan> daftarItem = new ArrayList<>();
+    //object array list bernama daftarItem 
     Scanner input = new Scanner(System.in);
+    //object scanner dengan nama input
+
     PerpustakaanView view = new PerpustakaanView();
+    //membuat objek dengan nama view
+
     int pilihanUser = 0;
+    //kondisi 0 awal 
   
 
     public void tampilkanMenu() {
@@ -24,26 +31,27 @@ public class PerpustakaanController {
             System.out.println("6. Hapus Item");
             System.out.println("7. Keluar");
 
-            System.out.println("Masukkan Pilihan Anda : ");
+            System.out.print("Masukkan Pilihan Anda : ");
             pilihanUser = input.nextInt();
             input.nextLine();
 
 
             switch (pilihanUser) {
-                case 1:
-                    System.out.println("Judul : ");
+                case 1: {
+                    System.out.print("Judul : ");
                     String judul = input.nextLine();
                     
-                    System.out.println("Kode : ");
+                    System.out.print("Kode : ");
                     String kode = input.nextLine();
                     
-                    System.out.println("Tahun : ");
+                    System.out.print("Tahun : ");
                     int  tahun = input.nextInt();
+                    input.nextLine();
                     
-                    System.out.println("Penulis : ");
+                    System.out.print("Penulis : ");
                     String  penulis = input.nextLine();
                     
-                    System.out.println("Halaman : ");
+                    System.out.print("Halaman : ");
                     int  jumlahHalaman = input.nextInt();
 
                     input.nextLine();
@@ -53,26 +61,86 @@ public class PerpustakaanController {
                     
                     
                     break;
+                }
+                  
             
                 
-                case 2:
+                case 2: 
+                {
+                    System.out.print("Judul : ");
+                    String judul = input.nextLine();
+
+                    System.out.print("Kode : ");
+                    String kode = input.nextLine();
+
+                    System.out.print("Tahun : ");
+                    int tahun = input.nextInt();
+                    input.nextLine();
+
+                    System.out.print("Edisi : ");
+                    String edisi = input.nextLine();
+
+                    System.out.print("Penerbit : ");
+                    String penerbit = input.nextLine();
+
+                    input.nextLine();
+
+                    MajalahPerpus objekItem = new MajalahPerpus(judul, kode, tahun, edisi, penerbit);
+                    daftarItem.add(objekItem);
+
+
+
                     break;
 
-                case 3:
-                    break;
-                case 4:
+                }
+
                     
+                case 3:
+                    view.tampilkanSemua(daftarItem);
+
+                    break;
+                case 4: {
+
+                    view.tampilkanSemua(daftarItem);
+
+                System.out.print("Nomor buku yang mau dipinjam : ");
+                int nomorUrut = input.nextInt();
+
+                daftarItem.get(nomorUrut - 1).dipinjam();
+  
                     break;
             
                 
+                }
+
+                
                 case 5:
+                    {
+                        view.tampilkanSemua(daftarItem);
+
+                        System.out.print("Nomor buku yang mau dikembalikan : ");
+                        int nomorUrut = input.nextInt();
+
+                        daftarItem.get(nomorUrut - 1).dikembalikan();
+                    }
                     break;
 
                 case 6:
+                    view.tampilkanSemua(daftarItem);
+
+                    System.out.print("Nomor buku yang mau dihapus : ");
+                    int nomorUrut = input.nextInt();
+
+                    daftarItem.remove(nomorUrut - 1);
+                    System.out.println("Buku telah dihapus !");
+
+
                     break;
                 
 
                 case 7:
+
+                view.tampilkanPesan("bat tenkyu ! ");
                     break;
                 
                 default:
